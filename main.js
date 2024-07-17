0954e66a3f6cdedb5e2a068e6c8f39a715299cf3
Akashnsk1 <106132129+Akashnsk1@users.noreply.github.com>
2024-07-17 12:52:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const contact_box =document.querySelector('#contact_box') ;
+
+let show = function(){
+    contact_box.style.display='block';
+}
+
+
+function toggleMenu() {
+    var x = document.getElementById("dropdown-content");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+document.querySelectorAll('.dropdown-content a').forEach(item => {
+    item.addEventListener('click', () => {
+        document.getElementById("dropdown-content").style.display = "none";
+    });
+});
+
+// Collapse the menu when clicking outside
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown-content')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
