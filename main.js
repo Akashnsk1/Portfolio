371855576f0450683dbd09c6079b4f3c5b448bf2
Akash <akashnsk9845@gmail.com>
2024-07-21 11:29:01 +0530

    Latest commit
    Resolve conflicts and complete rebase

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const contact_box =document.querySelector('#contact_box') ;
 
 let show = function(){
@@ -31,4 +32,39 @@ window.onclick = function (event) {
             }
         }
     }
+=======
+const contact_box =document.querySelector('#contact_box') ;
+
+let show = function(){
+    contact_box.style.display='block';
+}
+
+
+function toggleMenu() {
+    var x = document.getElementById("dropdown-content");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+document.querySelectorAll('.dropdown-content a').forEach(item => {
+    item.addEventListener('click', () => {
+        document.getElementById("dropdown-content").style.display = "none";
+    });
+});
+
+// Collapse the menu when clicking outside
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown-content')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+>>>>>>> 0f6a447 (Latest commit)
 }
